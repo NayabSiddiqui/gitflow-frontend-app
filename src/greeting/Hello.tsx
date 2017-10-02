c58879d77aefd35d01e56f0180ac2eee12fae2bd
Nayab Siddiqui <nayab.s86@gmail.com>
2017-10-03 00:08:27 +0530

    hey message

--- src/greeting/Hello.tsx
+++ src/greeting/Hello.tsx
@@ -17,7 +17,7 @@ const Hello = ({name, enthusiasmLevel = 1}: Props) => {
   return (
     <div className="hello">
       <div className="greeting">
-        Hi {name + getExclamationMarks(enthusiasmLevel)}
+        Hey {name + getExclamationMarks(enthusiasmLevel)}
       </div>
     </div>
   );
