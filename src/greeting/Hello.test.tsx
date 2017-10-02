676fe290dc8de16eb0c45e496a637292e39b90bd
Nayab Siddiqui <nayab.s86@gmail.com>
2017-10-02 19:56:54 +0530

    fixing failing tests

--- src/greeting/Hello.test.tsx
+++ src/greeting/Hello.test.tsx
@@ -9,17 +9,17 @@ import Hello from './Hello';
 
 it('should render the correct text when no entusiasm level is given', () => {
   const hello = enzyme.shallow(<Hello name='Daniel'/>);
-  expect(hello.find(".greeting").text()).toEqual('Hello Daniel!')
+  expect(hello.find(".greeting").text()).toEqual('Hey Daniel!')
 });
 
 it('renders the correct text with an explicit enthusiasm of 1', () => {
   const hello = enzyme.shallow(<Hello name='Daniel' enthusiasmLevel={1}/>);
-  expect(hello.find(".greeting").text()).toEqual('Hello Daniel!')
+  expect(hello.find(".greeting").text()).toEqual('Hey Daniel!')
 });
 
 it('renders the correct text with an explicit enthusiasm level of 5', () => {
   const hello = enzyme.shallow(<Hello name='Daniel' enthusiasmLevel={5} />);
-  expect(hello.find(".greeting").text()).toEqual('Hello Daniel!!!!!');
+  expect(hello.find(".greeting").text()).toEqual('Hey Daniel!!!!!');
 });
 
 it('throws when the enthusiasm level is 0', () => {
