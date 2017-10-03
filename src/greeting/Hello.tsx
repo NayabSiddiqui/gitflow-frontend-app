0e87d241757085eea1ab71658ea2e7eb11062524
Nayab Siddiqui <nayab.s86@gmail.com>
2017-10-03 20:11:12 +0530

    added welcome message

--- src/greeting/Hello.tsx
+++ src/greeting/Hello.tsx
@@ -17,7 +17,7 @@ const Hello = ({name, enthusiasmLevel = 1}: Props) => {
   return (
     <div className="hello">
       <div className="greeting">
-        Hola {name + getExclamationMarks(enthusiasmLevel)}
+        Hola {name + getExclamationMarks(enthusiasmLevel)}, Welcome!
       </div>
     </div>
   );
